81dcaff51c92fd163072060b4a1036f8096e1879
Andrew Eisenberg <aeisenberg@github.com>
2025-06-18 12:31:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 console.log("hi")
 
-// TODO sanitize the location search string by importing the sanitize library
-fetch(window.location.search).then(x => document.write(x));
+// TODO sanitize the string by importing the sanitize library
+const params = new URLSearchParams(window.location.search);
+const user = params.get("user");
+const welcome = document.querySelector("#welcome");
+
+welcome.innerHTML = `Welcome back, ${user}!`;
+
